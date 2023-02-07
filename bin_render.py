476390799834269778f09fe53496e1dd35dff6f7
hansaskov <hansaskov1999@gmail.com>
2023-02-07 23:04:37 +0100

    Added walls to guide components into bin

--- bin_render.py
+++ bin_render.py
@@ -10,7 +10,6 @@ import random
 from blenderproc.python.types.MeshObjectUtility import MeshObject
 from blenderproc.python.types.MaterialUtility import Material
 
-
 class Config:
     class Component:
         def __init__(self, json_data):
@@ -46,13 +45,13 @@ class Config:
 
 # Global variables
 vhacd_path = 'resources/vhacd'
-cache_path = 'resources/vhacd/decomp_cache'
+temp_path = vhacd_path + "/decomp_temp"
+cache_path = vhacd_path + '/decomp_cache'
 haven_path = 'resources/blenderproc/haven/'
 output_dir = 'data/'
 
-
 class Render:
-
+    
     class Component:
         def __init__(self, comp_config: Config.Component):
 
@@ -92,8 +91,7 @@ class Render:
             # Enable collision, create convex decomposition.
             self.obj = bproc.loader.load_obj(bin_config.path)[0]
             self.obj.enable_rigidbody(active=False, collision_shape="COMPOUND")
-            self.obj.build_convex_decomposition_collision_shape(
-                vhacd_path, cache_dir=cache_path)
+            self.obj.build_convex_decomposition_collision_shape(vhacd_path, temp_dir=temp_path, cache_dir=cache_path)
 
             dimensions = bin_config.dimensions
 
@@ -117,27 +115,18 @@ class Render:
             
     class Wall:
         def __init__(self, bin_shape):
-            # Load one wall
-
-
 
             self.planes = [
-                bproc.object.create_primitive('PLANE', scale=[bin_shape[0], 2, 1], location=[0, bin_shape[1]/2, 0], rotation=[-1.570796, 0, 0]),
-               bproc.object.create_primitive('PLANE', scale=[bin_shape[0], 2, 1], location=[0, -bin_shape[1]/2, 0], rotation=[1.570796, 0, 0]),
-               bproc.object.create_primitive('PLANE', scale=[2, bin_shape[1], 1], location=[bin_shape[0]/2,0 , 0], rotation=[0, -1.570796, 0]),
-               bproc.object.create_primitive('PLANE', scale=[2,bin_shape[1], 1], location=[-bin_shape[0]/2, 0, 0], rotation=[0, 1.570796, 0])
+                bproc.object.create_primitive('PLANE', scale=[bin_shape[0], 20, 1], location=[0, bin_shape[1]/2, 0], rotation=[-1.570796, 0, 0]),
+               bproc.object.create_primitive('PLANE', scale=[bin_shape[0], 20, 1], location=[0, -bin_shape[1]/2, 0], rotation=[1.570796, 0, 0]),
+               bproc.object.create_primitive('PLANE', scale=[20, bin_shape[1], 1], location=[bin_shape[0]/2,0 , 0], rotation=[0, -1.570796, 0]),
+               bproc.object.create_primitive('PLANE', scale=[20,bin_shape[1], 1], location=[-bin_shape[0]/2, 0, 0], rotation=[0, 1.570796, 0])
             ]
             for wall in self.planes:
                 wall.enable_rigidbody(active=False, collision_shape="COMPOUND")
                 wall.build_convex_decomposition_collision_shape(vhacd_path, cache_dir=cache_path)
+                wall.hide()
             
-        #def enable():
-            
-       # def disable():
-            
-            
-            
-
     def __init__(self, config: Config, args):
         bproc.init()
 
@@ -180,23 +169,22 @@ class Render:
         obj.set_rotation_euler(bproc.sampler.uniformSO3())
 
         # Get dimensions of the component and set longest x,y,z dimension
-        bounding_box = obj.get_bound_box().flatten()
-        length = max(abs(bounding_box))
+
 
         total_volume = sum(list(map(lambda comp: comp.volume * len(comp.obj_list), self.comps)))
-        box_volume = (self.bin.length_x - 2 * length) * (self.bin.length_y - 2 * length) * self.bin.length_z
+        box_volume = self.bin.length_x * self.bin.length_y * self.bin.length_z
 
         volume_frac = total_volume / box_volume
 
         obj.set_location(np.random.uniform(
             [
-                -self.bin.length_x/2 + length,
-                -self.bin.length_y/2 + length,
+                -(self.bin.length_x)/2.2,
+                -self.bin.length_y/2.2,
                 self.bin.length_z * 1.1
             ], [
-                self.bin.length_x/2 - length,
-                self.bin.length_y/2 - length,
-                self.bin.length_z * (1.1 + volume_frac * 3)
+                self.bin.length_x/2.2,
+                self.bin.length_y/2.2,
+                self.bin.length_z * 5 * volume_frac 
             ]
         ))
 
@@ -285,16 +273,15 @@ class Render:
 
             # Run the physics simulation
             bproc.object.simulate_physics_and_fix_final_poses(
-                min_simulation_time=1,
-                max_simulation_time=2,
+                min_simulation_time=2,
+                max_simulation_time=3,
                 check_object_interval= 0.5,
                 object_stopped_location_threshold = 0.01,
                 object_stopped_rotation_threshold = 5,
                 substeps_per_frame = 30,
                 solver_iters= 20,
             )
-
-            
+        
             # Change material of the bin
             if self.bin.random_color:
                bin_material = self.randomize_materials()
@@ -314,9 +301,6 @@ class Render:
 
             # Make lighting
             self.randomize_light()
-            
-            for wall in self.walls.planes:
-                wall.hide()
 
             # Make 4 random camera poses
             all_visible_comp = self.randomize_camera_poses(4)
@@ -345,8 +329,8 @@ class Render:
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--comp-amount-min',  nargs='?', default='3', help='The min amount of components that should be in the bin')
-    parser.add_argument('--comp-amount-max',  nargs='?', default='5', help='The max amount of components that can be in the bin')
+    parser.add_argument('--comp-amount-min',  nargs='?', default='2', help='The min amount of components that should be in the bin')
+    parser.add_argument('--comp-amount-max',  nargs='?', default='3', help='The max amount of components that can be in the bin')
     parser.add_argument('--number-of-runs',   nargs='?', default='1', help='The number of simulations you would like to do')
     parser.add_argument('--instance-num',     nargs='?', default='1', help='Give each component a different number')
     parser.add_argument('--config-path',     nargs='?', default='config.json', help='filepath to configuration JSON file')
